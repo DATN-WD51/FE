626e546518127ceece95caac40f059dbb3465c93
kimntm <nguyenthimykim659@gmail.com>
2025-10-30 17:47:45 +0700

    update homePage

---
+++ src/mock/Film.ts
@@ -0,0 +1,306 @@
+export const film_mock = [
+  {
+    _id: "69488390181face",
+    name: "Tay anh giữ một vì sao - k",
+    ageRequire: "K",
+    tag: ["Hài", "tâm lý", "tình cảm"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018933_0.jpg&w=3840&q=75",
+    language: "Phụ đề",
+    relaseDate: "06-10-2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "Tử chiến trên không - T16",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018944_0.jpg&w=640&q=75",
+    language: "Phụ đề",
+    relaseDate: "17-10-2025",
+  },
+  {
+    _id: "69488390181fac23e",
+    name: "Năm của anh, ngày của em - T13",
+    ageRequire: "13+",
+    tag: ["Tâm lý", "Tình cảm"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018983_0.jpg&w=640&q=75",
+    language: "Phụ đề",
+    relaseDate: "31-10-2025",
+  },
+  {
+    _id: "69488390181f12ace",
+    name: "Phá đám - Sinh nhật mẹ - T16",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019096_0.jpg&w=640&q=75",
+    language: "Phụ đề",
+    relaseDate: "31-10-2025",
+  },
+  {
+    _id: "69488390181f1223ace",
+    name: "Nhà ma xó - T16",
+    ageRequire: "16+",
+    tag: ["Kinh dị"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019130_0.jpg&w=640&q=75",
+    language: "Tiếng việt",
+    relaseDate: "24-10-2025",
+  },
+  {
+    _id: "69488390181f2723ace",
+    name: "Quỷ ăn tạng - T16",
+    ageRequire: "16+",
+    tag: ["Kinh dị"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019241_0.jpg&w=640&q=75",
+    language: "Phụ đề",
+    relaseDate: "10-10-2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "Chainsaw man - the movie: Chương reze - T18",
+    tag: ["Kinh dị"],
+    ageRequire: "18+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019134_0.jpg&w=640&q=75",
+    language: "Phụ đề",
+    relaseDate: "10-10-2025",
+  },
+  {
+    _id: "69419203890181f2723ac",
+    name: "CHỊ NGÃ EM NÂNG - T13",
+    tag: ["Hài", "Tâm lý"],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019172_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "03-10-2025",
+  },
+  {
+    _id: "69419203890181f2723ac",
+    name: "CỤC VÀNG CỦA NGOẠI-T13",
+    tag: ["Tình cảm", "Tâm lý"],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019271_0.jpg&w=384&q=75",
+    language: "Tiếng Việt, Phụ đề",
+    relaseDate: "17-10-2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "CẢI MẢ-T16",
+    tag: ["Kinh dị"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019171_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "31-10-2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "CHÚ THUẬT HỒI CHIẾN: HOÀI NGỌC / NGỌC CHIẾT – THE MOVIE -T13 (Phụ đề)",
+    tag: ["Hoạt hình"],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019240_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "10/10/2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "XÀ THUẬT TIỂU TAM-T16",
+    tag: ["Kinh dị"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019289_0.png&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "24/10/2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "TỚ VÀ ROBOCO: SIÊU CẤP ĐA VŨ TRỤ- P (Lồng Tiếng)",
+    tag: ["Hài", "Hoạt hình"],
+    ageRequire: "P",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019321_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "24/10/2025",
+  },
+];
+export const film_fes = [
+  {
+    _id: "69488390181face",
+    name: "Mọi thứ đều thuộc về mình + Bầu trời đỏ lửa - T13 - LHP Đức",
+    tag: [""],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019334_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "30/10/2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "Những lời chào từ sao hỏa - T16 - LHP Đức",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019330_0.jpg&w=384&q=75",
+    language: "Phụ đề, Tiếng Việt",
+    relaseDate: "02/11/2025",
+  },
+  {
+    _id: "69488390181fac23e",
+    name: "Quê hương là chùm khế chua + Giới cấm cuối cùng - T18 - LHP Đức",
+    ageRequire: "18+",
+    tag: ["Tâm lý", "Tình cảm"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019335_0.jpg&w=384&q=75",
+    language: "Phụ đề, Tiếng Việt",
+    relaseDate: "02/11/2025",
+  },
+  {
+    _id: "69488390181f12ace",
+    name: "Niko và chuyến phiêu lưu đến dải cực quang - P - LHP Đức",
+    tag: ["Hoạt hình", "Vui nhộn"],
+    ageRequire: "P",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019332_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "01/11/2025",
+  },
+  {
+    _id: "69488390181f1223ace",
+    name: "Mạch ngầm -T18- LHP Đức",
+    ageRequire: "18+",
+    tag: ["Tâm lý"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019333_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181f2723ace",
+    name: "Cologne 75 - T18 - LHP Đức",
+    ageRequire: "18+",
+    tag: ["Tâm lý"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019331_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "01/11/2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "Chỉ có ban đêm chúng ta mới có thể buồn + Nữ anh hùng - T13 - LHP Đức",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019329_0.jpg&w=384&q=75",
+    language: "Phụ đề, Tiếng Việt",
+    relaseDate: "02/11/2025",
+  },
+];
+export const film_coming = [
+  {
+    _id: "69488390181face",
+    name: "BỊT MẮT BẮT NAI - T18",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "18+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019245_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181fa112",
+    name: "CẢI MẢ-T16",
+    tag: ["Kinh dị"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019171_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181fac23e",
+    name: "THA RAE: MỤC SƯ, THẦY ĐỒNG VÀ CON QUỶ ÂM TRÌ-T18",
+    ageRequire: "18+",
+    tag: ["Kinh dị"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019346_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181f12ace",
+    name: "PHÁ ĐÁM - SINH NHẬT MẸ-T16",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "16+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019096_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181f1223ace",
+    name: "BÍ MẬT SAU BỮA TIỆC - T18",
+    ageRequire: "18+",
+    tag: ["Tâm lý", "Tình cảm"],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019322_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "31/10/2025",
+  },
+  {
+    _id: "69488390181f2723ace",
+    name: "QUÁI THÚ VÔ HÌNH: VÙNG ĐẤT CHẾT CHÓC",
+    ageRequire: "",
+    tag: [""],
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019211_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "07/11/2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "TÌNH NGƯỜI DUYÊN MA: NHẮM MAK YÊU LUÔN",
+    tag: ["Tâm lý", "Tình cảm"],
+    ageRequire: "",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019243_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "07/11/2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "GODZILLA MINUS ONE",
+    tag: ["Hành động"],
+    ageRequire: "13+",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019360_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "07/11/2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "TRỐN CHẠY TỬ THẦN",
+    tag: ["Hành động", "Phiêu lưu"],
+    ageRequire: "",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018993_0.jpg&w=384&q=75",
+    language: "Phụ đề",
+    relaseDate: "14/11/2025",
+  },
+  {
+    _id: "69419203890181f2723ace",
+    name: "HOÀNG TỬ QUỶ",
+    tag: ["Kinh dị"],
+    ageRequire: "",
+    thumbnail:
+      "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0019258_0.jpg&w=384&q=75",
+    language: "Tiếng Việt",
+    relaseDate: "29/11/2025",
+  },
+];
