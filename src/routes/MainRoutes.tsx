e6a9dc820debf62c09fe0f1e41c4b16a3883b974
Thanhtrung4 <trungntph40799@fpt.edu.vn>
2025-11-05 09:57:06 +0700

    update about

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,6 +1,7 @@
 import type { RouteObject } from "react-router";
 import MainLayout from "../common/layouts/MainLayout";
 import HomePage from "../pages/client/home/HomePage";
+import AboutPage from "../pages/about/AboutPage";
 export const MainRoutes: RouteObject[] = [
   {
     path: "",
@@ -9,6 +10,10 @@ export const MainRoutes: RouteObject[] = [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
       },
     ],
   },
