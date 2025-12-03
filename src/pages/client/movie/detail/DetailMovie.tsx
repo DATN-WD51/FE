3ef2fedb4951fd607244f7a1c21ffc47c08cf420
Thanhtrung4 <trungntph40799@fpt.edu.vn>
2025-12-03 22:57:05 +0700

    fix list room showtime in movie,detailmovie,mainrouter

--- src/pages/client/movie/detail/DetailMovie.tsx
+++ src/pages/client/movie/detail/DetailMovie.tsx
@@ -74,7 +74,7 @@ const DetailMovie = () => {
                   .join(", ")}
               </p>
               <p className="mt-2 line-clamp-5" title={movie?.description}>
-                Nội dung: ${movie?.description || "Chưa cập nhật"}
+                Nội dung: {movie?.description || "Chưa cập nhật"}
               </p>
             </div>
             <p className="text-[18px] text-primary line-clamp-1">
@@ -99,7 +99,7 @@ const DetailMovie = () => {
           </div>
         </div>
       </div>
-      <ShowtimePicker />
+      {movie?.statusRelease === "nowShowing" && <ShowtimePicker />}
     </div>
   );
 };
