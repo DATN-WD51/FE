e79207aa02a0cab49cc803a0bfcf4ac0556c316b
Trandong0001 <tdong6006@gmail.com>
2025-12-05 04:03:48 +0700

    Profile Page

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -4,9 +4,12 @@ import HomePage from "../pages/client/home/HomePage";
 import LoginGoogle from "../pages/client/auth/LoginGoogle";
 import VerifyUser from "../pages/client/auth/VerifyUser";
 import DetailMovie from "../pages/client/movie/detail/DetailMovie";
+import ShowtimePicker from "../pages/client/movie/detail/components/ShowtimePicker";
 import Checkout from "../pages/client/checkout/Checkout";
-import ShowtimePicker from "../pages/client/movie/detail/component/ShowtimePicker";
 import CheckoutResult from "../pages/client/checkout/CheckoutResult";
+import ProfileLayout from "../common/layouts/ProfileLayout";
+import Profile from "../pages/client/user/Profile";
+import MyTicket from "../pages/client/user/MyTicket";
 
 export const MainRoutes: RouteObject[] = [
   {
@@ -32,6 +35,20 @@ export const MainRoutes: RouteObject[] = [
       {
         path: "/checkout-result",
         element: <CheckoutResult />,
+      },
+      {
+        path: "profile",
+        element: <ProfileLayout />,
+        children: [
+          {
+            index: true,
+            element: <Profile />,
+          },
+          {
+            path: "ticket",
+            element: <MyTicket />,
+          },
+        ],
       },
     ],
   },
